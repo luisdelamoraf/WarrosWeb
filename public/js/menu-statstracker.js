0b567ad045c64854d8369164114d899721bf5d1f
luisdelamoraf <luise_delamora@hotmail.com>
2020-10-12 00:05:42 -0500

    agregar + editar + eliminar jugadores

--- public/js/menu-statstracker.js
+++ public/js/menu-statstracker.js
@@ -1,11 +1,16 @@
 localStorage = window.localStorage;
+let docPartido
+let jugadores
+// Crear Partido
 let partidoLiga = document.querySelector("#partidoLiga");
 let partidoEquipo = document.querySelector("#partidoEquipo");
 let partidoRival = document.querySelector("#partidoRival");
 let partidoFecha = document.querySelector("#partidoFecha");
-let docPartido
-let equipo
-let jugadores
+// Editar equipo
+let editarEquipo = document.querySelector("#editarEquipo");
+let editarNombre = document.querySelector("#editar-nombre-jugador");
+let editarNumero = document.querySelector("#editar-numero-jugador");
+let idJugador
 
 // Inicializar firebase/firestore
 const firebaseConfig = {
@@ -24,9 +29,58 @@ var firestore = firebase.firestore()
 // ./Inicializar firebase/firestore
 
 
+// Funciones
+function limpiarJugadores(){
+    $("#jugadores-activos").html(``)
+}
+function cargarEquipoEditar(){
+    limpiarJugadores()
+    let equipo = firestore.collection("equipos").doc(`${editarEquipo.value}`).collection("jugadores")
+    equipo.get().then(function(docs) {
+        docs.forEach(function(doc){
+            $("#jugadores-activos").append(`
+                <button type="button" onclick="EliminarJugador('${doc.id}')" class="btn btn-danger btn-sm btn-eliminar-jugador"> <i class="fa fa-trash" aria-hidden="true"></i></button>
+                <button type="button" onclick="EditarJugador('${doc.id}')" class="btn btn-secondary btn-sm btn-editar-jugador"> <i class="fa fa-pencil" aria-hidden="true"></i></button> ${doc.data().nombre} ${doc.data().numero} <hr>
+            `)
+        })
+    }).catch(function(error){
+        console.log("Error: ", error);
+    });
+}
+function EliminarJugador(id){
+    let equipo = firestore.collection("equipos").doc(`${editarEquipo.value}`).collection("jugadores")
+    equipo.doc(id).delete().then(function() {
+        alert("Jugador eliminado");
+        cargarEquipoEditar()
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    });
+}
+function EditarJugador(id){
+    idJugador = id
+    let equipo = firestore.collection("equipos").doc(`${editarEquipo.value}`).collection("jugadores")
+    equipo.doc(id).get().then(function(doc) {
+            $("#editar-nombre-jugador").val(doc.data().nombre)
+            $("#editar-numero-jugador").val(doc.data().numero)
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+    $("#regresar-agregar").show()
+    $("#btn-editar-jugador").show()
+    $("#btn-agregar-jugador").hide()
+}
+function regresarAgregar(){
+    $("#regresar-agregar").hide()
+    $("#btn-editar-jugador").hide()
+    $("#btn-agregar-jugador").show()
+    $("#editar-nombre-jugador").val("")
+    $("#editar-numero-jugador").val("")
+}
+
+// ./Funciones
 
 
-  $("#btn-nuevo-partido").click(function(){
+$("#btn-nuevo-partido").click(function(){
     let idPartido = partidoLiga.value +"_"+ partidoFecha.value +"_"+ Date.now()
     docPartido = firestore.doc(`ligas/all/${partidoLiga.value}/${idPartido}`)
     localStorage.setItem("Liga", partidoLiga.value)
@@ -36,11 +90,10 @@ var firestore = firebase.firestore()
         Rival: partidoRival.value, 
         Fecha: partidoFecha.value,
     }).then(function(){
-        console.log("Partido creado: "+ idPartido);
         idPartido++
         $('#modal-nuevo-partido').modal('hide');
         $('#modal-seleccion-jugadores').modal('show');
-        equipo = firestore.collection("equipos").doc(`${partidoEquipo.value}`).collection("jugadores")
+        let equipo = firestore.collection("equipos").doc(`${partidoEquipo.value}`).collection("jugadores")
         equipo.get().then(function(docs) {
             docs.forEach(function(doc){
                     $("#equipo-seleccionado").append(`<label><input type="checkbox" id="${doc.id}" value="${doc.id}"> ${doc.data().nombre} ${doc.data().numero}</label><hr>`)
@@ -71,6 +124,53 @@ $("#go-to-match").click(function(){
                 })
         }
     })
+    
     window.location.replace("https://warrosweb.web.app/statstracker.html");
+})
+
+$("#btn-editar-equipo").click(function(){
+    cargarEquipoEditar()
+});
+
+$("#btn-agregar-jugador").click(function(){
+    if(editarNombre.value && editarNumero.value){
+        let equipo = firestore.collection("equipos").doc(`${editarEquipo.value}`).collection("jugadores")
+        equipo.doc(editarEquipo.value+"_"+editarNumero.value+"_"+Date.now()).set({
+            nombre: editarNombre.value,
+            numero: editarNumero.value
+        }).then(function() {
+            alert("Jugador añadido al equipo");
+            cargarEquipoEditar()
+            $("#editar-nombre-jugador").val("")
+            $("#editar-numero-jugador").val("")
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
+    } else{
+        alert("Datos de jugador incompletos");
+    }
+})
+
+$("#btn-editar-jugador").click(function(){
+    if(editarNombre.value && editarNumero.value){ 
+        let equipo = firestore.collection("equipos").doc(`${editarEquipo.value}`).collection("jugadores") 
+        equipo.doc(idJugador).update({
+            nombre: editarNombre.value,
+            numero: editarNumero.value
+        }).then(function() {
+            alert("Jugador actualizado");
+            regresarAgregar()
+            cargarEquipoEditar()
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+    } else {
+        alert("Datos de jugador incompletos");
+    }
+})
+
+$("#regresar-agregar").click(function(){
+    regresarAgregar()
 })
 
